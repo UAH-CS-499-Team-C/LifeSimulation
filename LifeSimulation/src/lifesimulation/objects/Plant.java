a8088dfc0c9cd2aabdb8c8c94c09294f1b6d023f
Sam LaGrave <srl0011@uah.edu>
2020-02-24 12:08:59 -0600

    Starting plant reproduction

--- LifeSimulation/src/lifesimulation/objects/Plant.java
+++ LifeSimulation/src/lifesimulation/objects/Plant.java
@@ -5,6 +5,8 @@
  */
 package lifesimulation.objects;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import pkgLifeSimDataParser.LifeSimDataParser;
@@ -15,13 +17,18 @@ import pkgLifeSimDataParser.LifeSimDataParser;
  */
 public class Plant extends SimulationObject implements LivingCreature {
     
+    // Variables used for displaying and possibly other class
     private float diameter;
     private float height;
+    
+    // Variables used for other functions
     private float growthRate;
     private float maxSize;
     private float maxSeedNumber;
     private float maxSeedCastDistance;
     private float seedViability;
+    private ArrayList<Point2D.Float> seedLocations = new ArrayList<>();
+    private int secondsElapsed = 0;
 
     public Plant(float x, float y, float diameter) {
         super(x, y);
@@ -36,6 +43,14 @@ public class Plant extends SimulationObject implements LivingCreature {
         maxSeedNumber = (float)lsdp.getMaxSeedNumber();
         maxSeedCastDistance = (float)lsdp.getMaxSeedCastDistance();
         seedViability = (float)lsdp.getSeedViability();
+        // Save all viable seed locations
+        for(float i = Math.max(x - maxSeedCastDistance, 0.0f);
+                i <= Math.min(x + maxSeedCastDistance, lsdp.getWorldWidth()); i++){
+            for(float j = Math.max(y - maxSeedCastDistance, 0.0f);
+                j <= Math.min(y + maxSeedCastDistance, lsdp.getWorldHeight()); j++){
+                seedLocations.add(new Point2D.Float(i, j));
+            }
+        }
     }
 
     @Override
@@ -56,6 +71,20 @@ public class Plant extends SimulationObject implements LivingCreature {
                 diameter = maxSize;
             }
         }
+        
+        // Wait for reproduction
+        if(diameter == maxSize) {
+            secondsElapsed++;
+        }
+        
+        // Reproduce
+        if(secondsElapsed == 60) {
+            secondsElapsed = 0;
+            Reproduce();
+        }
+    }
+    
+    private void Reproduce() {
         
     }
     
