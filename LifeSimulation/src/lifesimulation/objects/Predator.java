179163cef1effb1d36367861c67d74ce3ac2e916
Sam LaGrave <srl0011@uah.edu>
2020-03-13 11:08:39 -0500

    Start on targetfinding

--- LifeSimulation/src/lifesimulation/objects/Predator.java
+++ LifeSimulation/src/lifesimulation/objects/Predator.java
@@ -5,6 +5,8 @@
  */
 package lifesimulation.objects;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import lifesimulation.utilities.Environment;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
@@ -27,7 +29,26 @@ public class Predator extends SimulationObject implements LivingCreature{
     private int maxOffspring;
     private float gestaion;
     private float offspringEnergy;
+    
+    private ArrayList<SimulationObject> allTargets;
+    private LivingCreature currentTarget;
 
+    /**
+     * Constructor for predator class
+     * @param x X location of the predator
+     * @param y Y location of the predator
+     * @param EU Predator's energy level
+     * @param genotype Predator's genotype
+     * @param maxSpeedHOD HOD predator's max speed
+     * @param maxSpeedHED HED predator's max speed
+     * @param maxSpeedHOR HOR predator's max speed
+     * @param maintainSpeed Predator's standard speed
+     * @param energyOutput Energy obtained from plant
+     * @param energyToReProduce Energy needed to reproduce
+     * @param maxOffspring Max number of offspring
+     * @param gestaion Time from start of reproduction to childbirth
+     * @param offspringEnergy Starting energy of the child
+     */
     public Predator(float x, float y, int EU, String genotype, float maxSpeedHOD, float maxSpeedHED, float maxSpeedHOR, float maintainSpeed, float energyOutput, float energyToReProduce, int maxOffspring, float gestaion, float offspringEnergy) {
         super(x, y);
         this.EU = EU;
@@ -42,6 +63,9 @@ public class Predator extends SimulationObject implements LivingCreature{
         this.gestaion = gestaion;
         this.offspringEnergy = offspringEnergy;
         this.collision = new Circle(x, y, 7);
+        
+        allTargets = new ArrayList<>();
+        currentTarget = null;
     }
 
     
@@ -53,19 +77,46 @@ public class Predator extends SimulationObject implements LivingCreature{
         g.setLineWidth(2);
         g.setColor(Color.black);
         g.draw(collision);
+        
+        allTargets.forEach(t -> {
+            g.drawLine(x, y, t.x, t.y);
+        });
     }
 
     @Override
     public void Update(Environment e) {
+        findTargets(e);
         
     }
     
+    /**
+     * Get the predator's energy level
+     * @return Current energy level
+     */
     public int getEnergy() {
         return EU;
     }
     
+    /**
+     * Get the predator's genotype
+     * @return Genotype string
+     */
     public String getGenotype() {
         return genotype;
+    }
+    
+    private void findTargets(Environment e){
+        e.getGrazers().forEach(g -> {
+            if(Point2D.distance(x, y, g.x, g.y) <= 150) {
+                allTargets.add(g);
+            }
+        });
+        
+        e.getPredators().forEach(p -> {
+            if(Point2D.distance(x, y, p.x, p.y) <= 150) {
+                allTargets.add(p);
+            }
+        });
     }
     
 }
